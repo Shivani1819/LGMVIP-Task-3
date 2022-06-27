d6234d371900b397d0e25932b38b4da425eabf87
Shivani kumari <103165848+Shivani1819@users.noreply.github.com>
2022-06-27 10:01:29 +0530

    Add files via upload

---
+++ style.css
@@ -0,0 +1,20 @@
+* {box-sizing: border-box;
+    font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    background-color:rgba(171, 75, 7, 0.774)
+}
+ 
+
+table{
+	margin-top: 50px;
+	text-align: center;
+	background: rgb(248, 8, 8);
+	color: #fff;
+}
+ 
+#show th, #show td{
+	padding: 13px;
+}
+
+#btna td input{
+	width: 100%;
+}
